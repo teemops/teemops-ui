24b92ea1f400dd3c0517950d6a8deb310d9fc2cf
kiwifellows <kiwifellows@gmail.com>
2019-01-21 20:12:00 +1300

    Added setup scripts, modified dev.json, added async module

---
+++ scripts/lib/file.js
@@ -0,0 +1,100 @@
+if (typeof Promise === 'undefined') {
+    var async = require('asyncawait/async');
+    var await = require('asyncawait/await');
+    var Promise = require('bluebird');
+} 
+const fs=require("fs");
+
+function init(){
+    
+}
+/**
+ * Reads file string
+ * 
+ * @param {*} fileName 
+ */
+function read(fileName){
+    return new Promise(function(resolve, reject){
+        fs.readFile(
+            fileName,
+            'utf8',
+            function(err, data){
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(data);
+                }
+        });
+    })
+}
+
+/**
+ * Output to file (replace all file content)
+ * 
+ * @param {*} content 
+ * @param {*} fileName 
+ */
+function write(content, fileName){
+    return new Promise(function(resolve, reject){
+        fs.writeFile(
+            fileName,
+            content,
+            function(err, data){
+                if(err){
+                    reject(err);
+                }else{
+                    resolve(data);
+                }
+        });
+    })
+}
+
+/**
+ * Updates config file value (assuming it is in json format)
+ * in the config directory
+ * Example:
+ * const success=await updateConfigItem('region', 'ap-southeast-1', '../app/config/config.json')
+ * @param {*} value 
+ * @param {*} configFile 
+ * @returns Promise<any>
+ */
+async function updateConfigItem(name, value, fileName){
+    try{
+        const fileStream=await read(fileName);
+        var jsonBuffer=JSON.parse(fileStream);
+        jsonBuffer[name]=value;
+        var content=JSON.stringify(jsonBuffer, null, 4);
+        const updateFile=await write(content, fileName);
+        return true;
+    }catch(e){
+        throw e;
+    }
+    
+}
+/**
+ * Gets config file value as object (type is variable)
+ * in the config directory
+ * Example:
+ * const success=await getConfigItem('region', '../app/config/config.json')
+ * @param {*} name 
+ * @param {*} fileName 
+ * @returns object
+ */
+async function getConfigItem(name, fileName){
+    try{
+        const fileStream=await read(fileName);
+        var jsonBuffer=JSON.parse(fileStream);
+        
+        return jsonBuffer[name];
+    }catch(e){
+        throw e;
+    }
+    
+}
+
+module.exports=init;
+module.exports.read=read;
+module.exports.write=write;
+module.exports.updateConfig=updateConfigItem;
+module.exports.getConfig=getConfigItem;
+
---
+++ scripts/setup.js
@@ -0,0 +1,107 @@
+/**
+ * Setup script which is used by main teemops setup.sh
+ * (https://github.com/teemops/teemops/blob/master/setup.sh)
+ * to generate config files values
+ * 
+ * Purpose:
+ * - change config.json values
+ * 
+ * Parameters:
+ * - config_source: A file which has config values to be updated
+ * - config_dest: Destination config files to be updated
+ * - spec_file: The location of a spec file of variables to be updated
+ * 
+ * node ../ui/scripts/setup.js teemops-app/teemops-serverless/conf/output.json ../ui/config/dev.json
+ * 
+ */
+const DEBUG_TIMEOUT=15000;
+/**
+ * Fields to update
+ */
+const fields=[
+    {
+        source: 'ServiceEndpoint',
+        dest: 'cloudapiEndpoint'
+    }
+];
+
+var file=require("./lib/file");
+
+if (typeof Promise === 'undefined') {
+    var async = require('asyncawait/async');
+    var await = require('asyncawait/await');
+    var Promise = require('bluebird');
+}
+
+if (process.argv.length<3){
+    console.log("Arguments need to be supplied as follows");
+    help();
+    process.exit();
+}
+
+if (process.argv[2]=="help"){
+    help();
+}
+/**
+ * args={
+ *  cwd: Current working directory script is running in,
+ *  source: Source file path for where config values come from,
+ *  output: Output folder where config values to be 
+ * }
+ */
+var args={
+    cwd: process.argv[1],
+    source: process.argv[2],
+    dest: process.argv[3]
+};
+
+function help(){
+    console.log(
+`
+node setup.js <source_config_path> <dest_config_path>
+`
+    )
+}
+
+const update_config=async function(source_item, dest_item, source, dest){
+    
+    var sourceFileValue=await file.getConfig(source_item, source);
+    //array of config hierarchical value (e.g. s3.app.something)
+    var full_dest_item=dest_item.toString().split(".");
+    //get top level value
+    var currentValue=await file.getConfig(full_dest_item[0], dest);
+    console.log(currentValue)
+    if(full_dest_item.length>=2){
+        console.log(full_dest_item);
+        console.log("FULL DEST ITEM!!!!")
+        currentValue[full_dest_item[1]]=sourceFileValue;
+    }else{
+        currentValue=sourceFileValue;
+    }
+    
+    const updateConfig=await file.updateConfig(full_dest_item[0], currentValue, dest);
+
+}
+console.log('waiting...');
+/**
+ * Updates the config file as follows:
+ * source: TopsMetaBucketName
+ * output: s3.app_bucket
+ * 
+ */
+setTimeout(function(){
+
+    fields.forEach(function(value, index, array){
+        update_config(value.source, value.dest, args.source, args.dest).then(function(){
+            console.log("configuration updated");
+        }).catch(function(error){
+            console.log(error);
+            console.error("ConfigError");
+        });
+    })
+    
+}, DEBUG_TIMEOUT);
+
+
+
+
